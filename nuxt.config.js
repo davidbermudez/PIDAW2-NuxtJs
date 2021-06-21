a20fdd29b96b4e6fbc2c21d63b4d90b55bb4bf15
davidbermudez <davidbermudez@jerez.es>
2021-06-21 13:04:36 +0200

    target server

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
-  target: 'static',
+  target: 'server',
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
